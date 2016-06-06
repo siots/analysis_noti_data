481a1897cd61901bdc02e621518f588fb509f370
Shinseungsou <wwww432@lycos.co.kr>
2016-06-07 05:44:38 +0900

    add to new date format.

--- parser_lib/dateformatter.py
+++ parser_lib/dateformatter.py
@@ -8,10 +8,20 @@ def format_time_app_usage(times):
     format_times = datetime.strptime(times, '%Y-%m-%d %H:%M:%S')
     return format_times
 
+def format_time_app_usage_eu(times):
+    times = times.encode("utf-8").replace("오전", "AM").replace("오후","PM")
+    format_times = datetime.strptime(times, '%m-%d-%Y %H:%M:%S')
+    return format_times
+
 
 def format_time_event_logger(times):
     times = times.encode("utf-8").replace("오전", "AM").replace("오후","PM")
     format_times = datetime.strptime(times, '%Y. %m. %d. %p %I:%M:%S')
+    return format_times
+
+def format_time_event_logger_eu(times):
+    times = times.encode("utf-8").replace("오전", "AM").replace("오후","PM")
+    format_times = datetime.strptime(times, '%b %d, %Y %I:%M:%S %p')
     return format_times
 
 
@@ -20,6 +30,10 @@ def format_time_hn(times):
     format_times = datetime.strptime(times, ' %Y-%m-%d %H:%M:%S')
     return format_times
 
+def format_time_another(times):
+    times = times.encode("utf-8").replace("오전", "AM").replace("오후","PM")
+    format_times = datetime.strptime(times, '%Y-%m-%d %H:%M:%S')
+    return format_times
 
 def formatdate_to_sec(format_times):
     secs = time.mktime(format_times.timetuple())
